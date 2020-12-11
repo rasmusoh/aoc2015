b1d777fde3004a72c046c9e7d5fd2cf8d821c61d
Rasmus Öhman Bruno <rasmus@ohmanbruno.se>
2020-12-11 15:01:53 +0100

    formatting

--- day3.py
+++ day3.py
@@ -2,7 +2,7 @@ import fileinput
 from operator import add
 
 dirs = {'>': [1, 0], '<': [-1, 0], '^': [0, 1], 'v': [0, -1]}
-for line in fileinput.input():
+for line in fileinput.input('data/day3.txt'):
     line = line.rstrip()
     pos = [(0, 0), (0, 0)]
     turn = 0
@@ -11,4 +11,4 @@ for line in fileinput.input():
         pos[turn] = tuple(map(add, pos[turn], dirs[char]))
         houses.add(pos[turn])
         turn = (turn + 1) % 2
-    print(houses)
+    print(len(houses))
